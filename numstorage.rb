4d8d1437471889e60014e4c0f7bdb716bec59b00
Sam Wolfe <sam.wolfe93@gmail.com>
2013-06-19 12:56:32 -0700

    number storage test

---
+++ numstorage.rb
@@ -0,0 +1,33 @@
+require 'rubygems'
+require 'sinatra'
+require 'twilio-ruby'
+require 'erb'
+
+client = Twilio::REST::Client.new 'ACf99cfbc0f42bb061e1dfed9ff6b168b4', 'f434ce9f001c0bc8cb770d85b6d861cd'
+twilio_number = '15148001174'
+
+numbers = [
+  {:index => 0, :number => '5149417619', :active => true}
+]
+
+nextIndex = 1
+
+get '/' do
+  erb :index, :locals => {
+    :numbers => numbers 
+  }
+end
+
+
+post '/addNumber' do
+  numbers.push({
+    :index => nextIndex,
+    :number => params[:From],
+    :active => true
+  })
+
+  nextIndex += 1
+
+  content_type 'text/xml'
+  erb :twiml
+end
