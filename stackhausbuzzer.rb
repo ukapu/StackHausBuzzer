05e4e0bdadc367a109d0aa9952740170e932affb
Sam Wolfe <sam.wolfe93@gmail.com>
2013-06-20 15:38:55 -0700

    changed method from get to post for voice requests

--- stackhausbuzzer.rb
+++ stackhausbuzzer.rb
@@ -22,7 +22,7 @@ get '/' do
   }
 end
 
-get '/buzzer' do
+post '/buzzer' do
   if params[:From] == gate_number || params[:From] == front_door_number || params[:From] == test
     Twilio::TwiML::Response.new do |r|
       numbers.each { |x| r.Dial x[:number] }
