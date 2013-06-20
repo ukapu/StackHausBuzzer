7fa152ee4f9245d0ef0e95a48749b3873b23e3c5
Sam Wolfe <sam.wolfe93@gmail.com>
2013-06-20 12:34:53 -0700

    removed test .rb files

--- rubytest.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'twilio-ruby'
-require 'sinatra'
-
-get '/' do
-  'supppp' + Twilio::VERSION
-
-end
-
--- twiml-quickstart.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'sinatra'
-require 'twilio-ruby'
-
-get '/hello-monkey' do
-  Twilio::TwiML::Response.new do |r|
-    r.Say 'Hello Monkey'
-  end.text
-end
