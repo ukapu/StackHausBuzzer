91589a66ada45904c213ee9252e7a5c9955f5b93
ukapu <sam.wolfe93@gmail.com>
2013-06-26 19:38:34 +0000

    switched over functionality to database

--- stackhausbuzzer.rb
+++ stackhausbuzzer.rb
@@ -13,32 +13,20 @@ twilio_number = '+15148001174'
 test = '+17782288756' 
 
 DB = Sequel.connect(ENV['DATABASE_URL'])
-
-def jwrite(object, file)
-  File.open(file, "w+") do |f|
-    JSON.dump(object,f)
-  end
+DB.create_table :numbers do
+  String :number
 end
-
-def jread(file)
-  File.open(file, "r") do |f|
-    j = JSON.load(f)
-  end
-end
-
-numbers = [ { :number => '+15149417619'} ]
+numbers = DB[:numbers]
 
 #jwrite(numbers, jfile)
 
 get '/' do
-  numbers = jread(jfile)
   erb :index, :locals => {
     :numbers => numbers
   }
 end
 
 post '/buzzer' do
-  numbers = jread(jfile)
   if params[:From] == ENV['GATE'] || params[:From] == ENV['FRONT_DOOR']  || params[:From] == test
     Twilio::TwiML::Response.new do |r|
       numbers.each { |x| r.Dial x[:number] }
@@ -47,25 +35,21 @@ post '/buzzer' do
 end
 
 post '/request' do
+  from = params[:From]
   content = params[:Body]
-  numbers = jread(jfile)
   if content == ENV['PIN']
-    if numbers.detect{|f| f["number"] == params[:From]} == nil
-      numbers.push({
-        "number" => params[:From]
-      })
+    if numbers.where(:number => from) == nil
+      numbers.insert(:number => from)
       message = "Your number has been added to the buzzer list. Press 9 when the gate calls to let yourself in!"
     else
       message = "Your number is already on the list."
     end
   elsif content.downcase == 'remove'
-    numbers = numbers.reject { |i| i["number"] == params[:From] }
+    numbers.where(:number => from).delete
     message = "Your number has been removed from the buzzer list."
   else
     message = "Whatever you were trying to do, it didn't work."
   end
-
-  jwrite(numbers, jfile)
 
   twiml = Twilio::TwiML::Response.new do |r|
     r.Sms message
