33b41cc1d0566caf81051d5ad586646190d06dd5
ukapu <sam.wolfe93@gmail.com>
2013-06-27 18:16:33 +0000

    added out of office message and 5 second timeout when dialing

--- stackhausbuzzer.rb
+++ stackhausbuzzer.rb
@@ -5,7 +5,7 @@ require 'erb'
 require 'json'
 require 'pp'
 require 'sequel'
-#require './env.rb'
+require './env.rb'
 
 jfile = "numbers.json"
 client = Twilio::REST::Client.new 'ACf99cfbc0f42bb061e1dfed9ff6b168b4', 'f434ce9f001c0bc8cb770d85b6d861cd'
@@ -21,11 +21,19 @@ get '/' do
 end
 
 post '/buzzer' do
+
   if params[:From] == ENV['GATE'] || params[:From] == ENV['FRONT_DOOR']  || params[:From] == test
-    Twilio::TwiML::Response.new do |r|
-      numbers.each { |x| r.Dial x[:number] }
-    end.text
+    if Time.now.hour < 18 || Time.now.hour > 8
+      Twilio::TwiML::Response.new do |r|
+        numbers.each { |x| r.Dial x[:number], :timeout => "5" }
+      end.text
+    else
+      Twilio::TwiML::Response.new do |r|
+        r.Say 'We are currently closed. Come back during business hours.', :voice => 'woman'
+      end.text
+    end
   end
+
 end
 
 post '/request' do
@@ -33,7 +41,7 @@ post '/request' do
   content = params[:Body]
   if content == ENV['PIN']
     if numbers.where(:number => from).count == 0 
-      numbers.insert(:number => from)
+      numbers.insert(:number => from) #, :time => Time.now)
       message = "Your number has been added to the buzzer list. Press 9 when the gate calls to let yourself in!"
     else
       message = "Your number is already on the list."
